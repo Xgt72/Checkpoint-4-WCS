6423ee707ddcea996eb917b2aa9711ba94426b61
Xavier Georget <xageorget@gmail.com>
2019-07-16 15:48:55 +0200

    update of app-routing.module

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { BuyTicketsComponent } from "./buy-tickets/buy-tickets.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
+import { FindUsComponent } from "./find-us/find-us.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { PerformancesComponent } from "./performances/performances.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'AboutUs', pathMatch: 'full' },
+  { path: 'AboutUs', component: AboutUsComponent },
+  { path: 'Performances', component: PerformancesComponent },
+  { path: 'BuyTickets', component: BuyTicketsComponent },
+  { path: 'FindUs', component: FindUsComponent },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'ContactUs', component: ContactUsComponent },
+  { path: 'admin', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FindUsComponent } from './find-us/find-us.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     FindUsComponent,
     GalleryComponent,
     ContactUsComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
